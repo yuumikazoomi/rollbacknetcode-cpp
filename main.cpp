0fbf41fb34a16e43821ea7c9f6b9593b8c747882
Irelia <irelia@Irelias-MacBook-Pro.local>
2022-05-16 10:03:47 -0400

    drawing

--- main.cpp
+++ main.cpp
@@ -68,15 +68,5 @@ void update(){
      */
 }
 void draw(SDL_Renderer* renderer){
-/*
-    SDL_SetRenderDrawColor( renderer, 245, 245, 245, 255 );
-    SDL_RenderClear(renderer);
-    
-    
-    SDL_Rect r = {50,50,50,50};
-    SDL_SetRenderDrawColor( renderer, 255, 161, 0, 255 );
-    SDL_RenderFillRect( renderer, &r );
-    
-    SDL_RenderPresent(renderer);
- */
+    g->draw(renderer);
 }
--- stage.cpp
+++ stage.cpp
@@ -4,5 +4,13 @@ Stage::Stage(GameState* state){
     this->state = state;
 }
 void Stage::draw(SDL_Renderer* renderer){
+    SDL_SetRenderDrawColor( renderer, 245, 245, 245, 255 );
+    SDL_RenderClear(renderer);
     
+    
+    SDL_Rect r = {50,50,50,50};
+    SDL_SetRenderDrawColor( renderer, 255, 161, 0, 255 );
+    SDL_RenderFillRect( renderer, &r );
+    
+    SDL_RenderPresent(renderer);
 }
